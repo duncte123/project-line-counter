8caa69c905dbdb99270f525642ccb75c56e4fc9e
duncte123 <duncan.sterken123@gmail.com>
2020-01-04 11:23:46 +0100

    Shadow jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,14 @@
 plugins {
     java
     application
+
+    id("com.github.johnrengelman.shadow") version "5.0.0"
 }
 
 group = "me.duncte123"
 version = "1.0-SNAPSHOT"
+
+val shadowJar: com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar by tasks
 
 application {
     mainClassName = "me.duncte123.linecounter.Main"
@@ -20,4 +24,9 @@ dependencies {
 
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_11
+}
+
+shadowJar.apply {
+    archiveClassifier.set("")
+    archiveVersion.set("")
 }
